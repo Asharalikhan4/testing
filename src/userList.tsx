88293734d9cb97559d30a95c55b79b1113839aa3
Asharalikhan4 <contactashar4@gmail.com>
2025-09-29 21:03:26 +0530

    types added for deployment issue

--- src/userList.tsx
+++ src/userList.tsx
@@ -1,10 +1,19 @@
 import React from "react";
 
-const UserList = React.memo(({ searchData }) => {
+interface User {
+  id: number;
+  login: string;
+}
+
+interface UserListProps {
+  searchData: User[];
+}
+
+const UserList = React.memo(({ searchData }: UserListProps) => {
   return (
     <div>
-      {searchData?.map((user) => (
-        <div key={user?.id}>{user?.id}</div>
+      {searchData?.map((user: User) => (
+        <div key={user?.id}>{user?.login}</div>
       ))}
     </div>
   );
